14c37f516423d830cbc90424176e5dc582e1594a
haziq <haziq.othman99@gmail.com>
2020-09-30 17:50:18 +0800

    feat: Header

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+                src="https://www.ephotozine.com/newsletter/wh-august_1028/Amazon-WH-Banner.jpg"
+                alt="" />
+        </div>
+    )
+}
+
+export default Home;
